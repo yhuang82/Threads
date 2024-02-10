153991cdf161c6afff5ed170ca57c81745755859
Yiqun Huang Albert <albert913388672@gmail.com>
2024-02-09 17:38:00 -0800

    fix the issues need go to hte sign-in page to follow the demo

--- threads-clone/app/(root)/page.tsx
+++ threads-clone/app/(root)/page.tsx
@@ -2,7 +2,7 @@ import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div className="h-screen">
+    <div>
       <UserButton afterSignOutUrl="/" />
     </div>
   );
