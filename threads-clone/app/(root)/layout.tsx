0e7d155a78ba843f6f677b79c0a9b471d04f35d5
Yiqun Huang Albert <albert913388672@gmail.com>
2024-02-16 23:59:25 -0800

    ss

--- threads-clone/app/(root)/layout.tsx
+++ threads-clone/app/(root)/layout.tsx
@@ -24,7 +24,6 @@ export default function RootLayout({
       <html lang="en">
         <body className={inter.className}>
           <Topbar />
-
           <main>
             <LeftSidebar />
             <section className="main-container">
