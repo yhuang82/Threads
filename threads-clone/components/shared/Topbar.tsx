5bb4a0280327652bd1f5dd69a3254b2bccfeafbb
Yiqun Huang Albert <albert913388672@gmail.com>
2024-02-21 00:54:55 -0800

    finish the top bar, some render issues still need to solve for the organizationswitcher, wait for later appearance

--- threads-clone/components/shared/Topbar.tsx
+++ threads-clone/components/shared/Topbar.tsx
@@ -1,3 +1,4 @@
+import { OrganizationSwitcher, SignOutButton, SignedIn } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -6,13 +7,30 @@ function Topbar() {
     <nav className="topbar">
       <Link href="/" className="flex items-center gap-4">
         <Image src="/assets/logo.svg" alt="logo" width={28} height={28} />
-        <p className="text-heading3-bold text-light-1 max-xs:hidden">
-          Threads
-        </p>
+        <p className="text-heading3-bold text-light-1 max-xs:hidden">Threads</p>
       </Link>
-      <div className="flex items-center gap-4">
+      <div className="flex items-center gap-1">
+        <div className="block md:hidden">
+          <SignedIn>
+            <SignOutButton>
+              <div className="flex cursor-pointer">
+                <Image src="/assets/logout.svg" alt="logout" width={24} height={24} />
+              </div>
+            </SignOutButton>
+          </SignedIn>
+        </div>
+        
+        <OrganizationSwitcher
+          appearance={{
+            elements: {
+              OrganizationSwitcherTrigger:
+                "py-2 px-4"
+            }
+          }}
+          />
+      </div>
     </nav>
-  )
+  );
 }
 
 export default Topbar;
