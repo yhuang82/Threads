af2c03716a5ed28b885bf95b24208e9756c1e992
Yiqun Huang Albert <albert913388672@gmail.com>
2024-02-21 23:59:31 -0800

    leftsidebar

--- threads-clone/components/shared/LeftSidebar.tsx
+++ threads-clone/components/shared/LeftSidebar.tsx
@@ -1,5 +1,21 @@
+import {sidebarLinks} from '@/constants';
+import Link from 'next/link';
+
 function LeftSidebar() {
-  return <h1>LeftSidebar</h1>;
+  return (
+    <section className="custom-scrollbar leftsidebar">
+      <div className="flex w-full flex-1 flex-col gap-6 px-6">
+        {sidebarLinks.map((link) => (
+          <Link
+            href={link.route}
+            key={link.label}
+          >
+            Link
+          </Link>
+        ))}
+      </div>
+    </section>
+  );
 }
 
 export default LeftSidebar;
