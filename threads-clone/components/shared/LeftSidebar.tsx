affa7468ba1f22dc404f6c32aad199afb4dd655b
Yiqun Huang Albert <albert913388672@gmail.com>
2024-02-23 23:59:04 -0800

    leftsidebar

--- threads-clone/components/shared/LeftSidebar.tsx
+++ threads-clone/components/shared/LeftSidebar.tsx
@@ -1,17 +1,23 @@
-import {sidebarLinks} from '@/constants';
+"use client";
+
+import { sidebarLinks } from '@/constants';
 import Link from 'next/link';
 import Image from 'next/image';
+import { usePathname, useRouter} from 'next/navigation';
 function LeftSidebar() {
+  const router = useRouter();
+  const pathname = usePathname();
   return (
     <section className="custom-scrollbar leftsidebar">
       <div className="flex w-full flex-1 flex-col gap-6 px-6">
         {sidebarLinks.map((link) => {
-          const isActive = link.route === window.location.pathname;
+          const isActive = (pathname.includes(link.route) && link.route.length > 1) || pathname === link.route;
+          
           return (
             <Link
               href={link.route}
               key={link.label}
-              className='leftsidebar_link'
+              className={`leftsidebar_link ${isActive && 'bg-primary-500'}`}
             >
               <Image
                 src={link.imgURL}
