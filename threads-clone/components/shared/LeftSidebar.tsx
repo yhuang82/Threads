d6af3243cf3909e6ca64533c2ba0163465f2f061
Yiqun Huang Albert <albert913388672@gmail.com>
2024-02-24 23:59:06 -0800

    leftside bar logout didnot show there is a bug

--- threads-clone/components/shared/LeftSidebar.tsx
+++ threads-clone/components/shared/LeftSidebar.tsx
@@ -3,7 +3,8 @@
 import { sidebarLinks } from '@/constants';
 import Link from 'next/link';
 import Image from 'next/image';
-import { usePathname, useRouter} from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation'; import { SignOutButton, SignedIn } from "@clerk/nextjs";
+
 function LeftSidebar() {
   const router = useRouter();
   const pathname = usePathname();
@@ -11,13 +12,15 @@ function LeftSidebar() {
     <section className="custom-scrollbar leftsidebar">
       <div className="flex w-full flex-1 flex-col gap-6 px-6">
         {sidebarLinks.map((link) => {
-          const isActive = (pathname.includes(link.route) && link.route.length > 1) || pathname === link.route;
-          
+          const isActive =
+            (pathname.includes(link.route) && link.route.length > 1) ||
+            pathname === link.route;
+
           return (
             <Link
               href={link.route}
               key={link.label}
-              className={`leftsidebar_link ${isActive && 'bg-primary-500'}`}
+              className={`leftsidebar_link ${isActive && "bg-primary-500"}`}
             >
               <Image
                 src={link.imgURL}
@@ -26,10 +29,26 @@ function LeftSidebar() {
                 height={24}
               />
 
-              <p className='text-light-1 max-lg:hidden'>{link.label}</p>
+              <p className="text-light-1 max-lg:hidden">{link.label}</p>
             </Link>
-          )}
-        )}
+          );
+        })}
+      </div>
+      <div className="mt-10 px-6">
+        <SignedIn>
+          <SignOutButton>
+            <div className="flex cursor-pointer">
+              <Image
+                src="/assets/logout.svg"
+                alt="logout"
+                width={24}
+                height={24}
+              />
+
+              <p className="text-light-2 max-lg:hidden">Logout</p>
+            </div>
+          </SignOutButton>
+        </SignedIn>
       </div>
     </section>
   );
