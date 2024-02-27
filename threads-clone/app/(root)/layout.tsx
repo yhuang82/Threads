4a25cf4b463e16a33eabba79553d6848d57aeef6
Yiqun Huang Albert <albert913388672@gmail.com>
2024-02-26 23:10:22 -0800

    finished the home page now

--- threads-clone/app/(root)/layout.tsx
+++ threads-clone/app/(root)/layout.tsx
@@ -24,12 +24,10 @@ export default function RootLayout({
       <html lang="en">
         <body className={inter.className}>
           <Topbar />
-          <main>
+          <main className="flex flex-row">
             <LeftSidebar />
             <section className="main-container">
-              <div className="w-full max-w-4xl">
-                {children}
-              </div>
+              <div className="w-full max-w-4xl">{children}</div>
             </section>
             <RightSidebar />
           </main>
