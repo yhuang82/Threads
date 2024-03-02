8c3ad1182568fc74eae97bcbee0d005b2b0a4350
Yiqun Huang Albert <albert913388672@gmail.com>
2024-03-02 00:46:06 -0800

    ready to build the accountprofile page

--- threads-clone/components/forms/AccountProfile.tsx
+++ threads-clone/components/forms/AccountProfile.tsx
@@ -1,11 +1,18 @@
-"use client"
-
-const AccountProfile = () => {
-  return (
-    <div>
-      Account Profile
-    </div>
-  )
+"use client";
+interface Props {
+  user: {
+    id: string;
+    objectId: string;
+    username: string;
+    name: string;
+    bio: string;
+    image: string;
+  };
+  btnTitle: string;
 }
+  
+const AccountProfile = ({user, btnTitle}: Props) => {
+  return <div>Account Profile</div>;
+};
 
-export default AccountProfile
+export default AccountProfile;
