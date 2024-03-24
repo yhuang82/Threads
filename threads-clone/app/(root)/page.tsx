e7838299cfe45021b2978300ae320465ba3406c2
Yiqun Huang Albert <albert913388672@gmail.com>
2024-03-24 01:23:36 -0700

    fetch post the backend finish

--- threads-clone/app/(root)/page.tsx
+++ threads-clone/app/(root)/page.tsx
@@ -1,10 +1,12 @@
 import { UserButton } from "@clerk/nextjs";
 // page initial
-export default function Home() {
+export default async function Home() {
+  // const result = await fetchPosts();
+
   return (
     <>
       <h1 className="head-text text-left">Home</h1>
     </>
-  )
+  );
 }
 // page initial
--- threads-clone/lib/actions/thread.actions.ts
+++ threads-clone/lib/actions/thread.actions.ts
@@ -5,7 +5,6 @@ import Thread from "../models/thread.model";
 import User from "../models/user.model";
 import { connectToDB } from "../mongoose";
 
-
 // action to create a new thread
 interface Params {
   text: string;
@@ -38,4 +37,36 @@ export async function createThread({
   } catch (error: any) {
     throw new Error(`Error create thread: ${error.message}`);
   }
+}
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+  connectToDB();
+
+  //Calculate the number of posts to skip
+  const skipAmount = pageSize * (pageNumber - 1);
+
+  // Fetch the posts that have no parents (top-level threads ...)
+  const postsQuery = Thread.find({ parentId: { $in: [null, undefined] } })
+    .sort({ createdAt: "desc" })
+    .skip(skipAmount)
+    .limit(pageSize)
+    .populate({ path: "author", model: User })
+    .populate({
+      path: "children",
+      populate: {
+        path: "author",
+        model: User,
+        select: "_id name parentId image",
+      },
+    });
+
+  const totalPostsCount = await Thread.countDocuments({
+    parentId: { $in: [null, undefined] },
+  });
+
+  const posts = await postsQuery.exec();
+
+  const isNext = totalPostsCount > skipAmount + posts.length;
+
+  return {posts, isNext}
 }
