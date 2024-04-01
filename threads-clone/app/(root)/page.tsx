727dfc2bd1a6c952b814fe148a91fb8966b9dfe7
Yiqun Huang Albert <albert913388672@gmail.com>
2024-03-31 23:59:48 -0700

    home

--- threads-clone/app/(root)/page.tsx
+++ threads-clone/app/(root)/page.tsx
@@ -1,11 +1,30 @@
-import { UserButton } from "@clerk/nextjs";
+import { fetchPosts } from "@/lib/actions/thread.actions";
+import { currentUser } from "@clerk/nextjs";
 // page initial
 export default async function Home() {
-  // const result = await fetchPosts();
+  const result = await fetchPosts(1, 30);
+  const user = await currentUser();
 
   return (
     <>
       <h1 className="head-text text-left">Home</h1>
+
+      <section className="mt-9 flex flex-col gap-10">
+        {result.posts.length === 0 ? (
+          <p className="no-result">No threads found</p>
+        ) : (
+          <>
+            {result.posts.map((post) => (
+              <ThreadCard
+                key={post._id}
+                id={post._id}
+                currentUserId={user?.id}
+                parentId={post.parentId}
+              />
+            ))}
+          </>
+        )}
+      </section>
     </>
   );
 }
