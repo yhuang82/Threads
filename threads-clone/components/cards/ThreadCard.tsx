045114ed4b47ac29e3d08ca0a73c9f6446a2a238
Yiqun Huang Albert <albert913388672@gmail.com>
2024-04-09 00:00:03 -0700

    find a bug for create a thread showed two

--- threads-clone/components/cards/ThreadCard.tsx
+++ threads-clone/components/cards/ThreadCard.tsx
@@ -34,6 +34,7 @@ const ThreadCard = ({
   community,
   createdAt,
   comments,
+  isComment,
 }: Props) => {
   return (
     <article className="flex w-full flex-col rounded-xl bg-dark-2 p-7">
@@ -69,15 +70,15 @@ const ThreadCard = ({
                   height={24}
                   className="cursor-pointer object-contain"
                 />
-                <Link>
+                <Link href={`/thread/${id}`}>
+                  <Image
+                    src="/assets/reply.svg"
+                    alt="reply"
+                    width={24}
+                    height={24}
+                    className="cursor-pointer object-contain"
+                  />
                 </Link>
-                <Image
-                  src="/assets/reply.svg"
-                  alt="reply"
-                  width={24}
-                  height={24}
-                  className="cursor-pointer object-contain"
-                />
                 <Image
                   src="/assets/repost.svg"
                   alt="repost"
@@ -93,6 +94,14 @@ const ThreadCard = ({
                   className="cursor-pointer object-contain"
                 />
               </div>
+
+              {isComment && comments.length > 0 && (
+                <Link href={`/thread/${id}`}>
+                  <p className="mt-1 text-subtle-medium text-gray-1">
+                    {comments.length} replies
+                  </p>
+                </Link>
+              )}
             </div>
           </div>
         </div>
