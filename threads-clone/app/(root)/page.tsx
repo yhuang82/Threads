de08a88fb499fcfdded0b71b302ee0bd77920049
Yiqun Huang Albert <albert913388672@gmail.com>
2024-04-11 23:59:32 -0700

    continus

--- threads-clone/app/(root)/page.tsx
+++ threads-clone/app/(root)/page.tsx
@@ -1,7 +1,7 @@
 import ThreadCard from "@/components/cards/ThreadCard";
 import { fetchPosts } from "@/lib/actions/thread.actions";
 import { currentUser } from "@clerk/nextjs";
-// page initial
+// page initial*
 export default async function Home() {
   const result = await fetchPosts(1, 30);
   const user = await currentUser();
